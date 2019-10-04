33822fab8aafcef6ecc113c7525a6df8283aada0
Brandon-Harris <brandonharris177@gmail.com>
2019-10-04 09:17:52 -0600

    index displaying

--- index.js
+++ index.js
@@ -12,3 +12,25 @@ I need this code, just don't know where, perhaps should make some middleware, do
 
 Go code!
 */
+
+const express = require('express');
+
+// const data = require('./data/db');
+// const router = require('./Router/Router')
+
+const server = express();
+
+server.use(express.json());
+
+// server.use('/api/posts', router);
+
+port = 5000
+  
+// watch for connections on port 5000
+server.listen(port, () =>
+console.log(`Server running on http://localhost:${port}`)
+);
+
+server.get('/', (req, res) => {
+    res.send('Its working from index');
+  });
